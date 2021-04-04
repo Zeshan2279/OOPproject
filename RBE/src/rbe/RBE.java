89d710c46be8a83b9b940725bbd9052c4ca8e8b8
Zeshan Ayub <unofficialmz@gmail.com>
2021-04-04 10:35:59 +0500

    Project Created
    New Project has been created with only one java main file.

---
+++ RBE/src/rbe/RBE.java
@@ -0,0 +1,16 @@
+package rbe;
+
+/**
+ *
+ * @author YDRGZM
+ */
+public class RBE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
