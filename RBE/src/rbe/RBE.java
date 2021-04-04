59f35046bb18d84da07fd3650483e2a9f03ca2ab
Zeshan Ayub <unofficialmz@gmail.com>
2021-04-04 15:25:39 +0500

    SwingX jar
    Added swingX jar file in class path and module path in project

--- RBE/src/rbe/RBE.java
+++ RBE/src/rbe/RBE.java
@@ -5,12 +5,10 @@ package rbe;
  * @author YDRGZM
  */
 public class RBE {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
